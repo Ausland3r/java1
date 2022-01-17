ab346b913a33856b3d8048ad6eac82f5b12f7517
Ausland3r <Ausland3r@yandex.ru>
2022-01-17 16:53:57 +0300

    m and x

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
-
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Aboba");
+        int[] m = {};
+        double[]x = {};
+        double[][]n = new double[12][15];
+        for(int i = 3; i <= 25; i++)
+        {
+            m = Arrays.copyOf(m, m.length + 1);
+            m[m.length - 1] = i;
+        }
+        for(int i = 1; i <= 15; i++)
+        {
+            x =  Arrays.copyOf(x, x.length + 1);
+            x[x.length - 1] = rnd(-6, 8);
+        }
+        System.out.println(Arrays.toString(m));
+        System.out.println(Arrays.toString(x));
     }
+
+    public static double rnd(double min, double max)
+    {
+        return (double) (Math.random() * (max - min + 1)) + min;
+    }
+//    public static double getValue(int i,int  j)
+//    {
+//        for(int a = 0; a < 12; a++)
+//        {
+//
+//        }
+//    }
 }
